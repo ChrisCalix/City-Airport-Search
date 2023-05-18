49f52bb3f3bcf4782b9bd6ccc863f6c0accb28b4
christian.sonic <yoh.chrisitan@gmail.com>
2023-05-18 16:07:58 -0400

    feature/SearchCity/ViewModel
    Create Input and ouput tuples to comunicate in Typealias and create properties of this dinamyc tuples preventing to create multples properties for any specif case

--- City Airport Search/ViewModels/SearchCityViewModel.swift
+++ City Airport Search/ViewModels/SearchCityViewModel.swift
@@ -5,12 +5,30 @@
 //  Created by Sonic on 18/5/23.
 //
 
-import Foundation
+import RxCocoa
 
 protocol SearchCityViewModelPresentable {
+    typealias Input = (
+        searchText: Driver<String>, ()
+    )
+    typealias Ouput = ()
     
+    var input: SearchCityViewModelPresentable.Input { get }
+    var output: SearchCityViewModelPresentable.Ouput { get }
 }
 
 final class SearchCityViewModel: SearchCityViewModelPresentable {
+    var input: SearchCityViewModelPresentable.Input
+    var output: SearchCityViewModelPresentable.Ouput
     
+    init(input: SearchCityViewModelPresentable.Input) {
+        self.input = input
+        self.output = SearchCityViewModel.output(input: input)
+    }
+}
+
+private extension SearchCityViewModel {
+    static func output(input: SearchCityViewModelPresentable.Input) {
+        return ()
+    }
 }
