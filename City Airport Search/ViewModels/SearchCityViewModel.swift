4721f74d4141fb80342d8dc83a41a8b0de68d7a1
christian.sonic <yoh.chrisitan@gmail.com>
2023-05-18 15:59:54 -0400

    feature/searchCity/UIpresentation
    create UI design in uI kit to add textfield to search and tableview empty to list results, create MVVM file for this screen using protocol inverting the dependency in the View and apply Linskow sustitute principle.

---
+++ City Airport Search/ViewModels/SearchCityViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  SearchCityViewModel.swift
+//  City Airport Search
+//
+//  Created by Sonic on 18/5/23.
+//
+
+import Foundation
+
+protocol SearchCityViewModelPresentable {
+    
+}
+
+final class SearchCityViewModel: SearchCityViewModelPresentable {
+    
+}
---
+++ City Airport Search/Views/SearchCityViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SearchCityViewController.swift
+//  City Airport Search
+//
+//  Created by Sonic on 18/5/23.
+//
+
+import UIKit
+import RxSwift
+import RxDataSources
+
+final class SearchCityViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var contentSearchView: UIView!
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    var viewModel: SearchCityViewModelPresentable!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
