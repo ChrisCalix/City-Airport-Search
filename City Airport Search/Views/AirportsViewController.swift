1fc88170e31d0fb35d163319642c9b62e35adacf
christian.sonic <yoh.chrisitan@gmail.com>
2023-05-19 00:00:35 -0400

    feature/airports/cell
    adding cells implementation for airports

--- City Airport Search/Views/AirportsViewController.swift
+++ City Airport Search/Views/AirportsViewController.swift
@@ -14,7 +14,7 @@ class AirportsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+       
     }
     
 }
---
+++ City Airport Search/Views/Cells/AirportTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  AirportTableViewCell.swift
+//  City Airport Search
+//
+//  Created by Sonic on 19/5/23.
+//
+
+import UIKit
+
+class AirportTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
