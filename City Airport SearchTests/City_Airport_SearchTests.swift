aefcd5fe311f32a650cbf9349da277fe1cbf2bdf
christian.sonic <yoh.chrisitan@gmail.com>
2023-05-19 03:22:29 -0400

    fix test file

--- City Airport SearchTests/City_Airport_SearchTests.swift
+++ City Airport SearchTests/City_Airport_SearchTests.swift
@@ -27,9 +27,7 @@ final class City_Airport_SearchTests: XCTestCase {
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
+
     }
 
 }
